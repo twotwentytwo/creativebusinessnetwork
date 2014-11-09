a191c511a874767da6d0f072e4ae20baeeab6a92
Tom Kershaw <tmkersh@gmail.com>
2014-11-09 18:40:59 +0000

    editing company profile layout for use with grid structure.

--- webroot/app/views/companies/show.blade.php
+++ webroot/app/views/companies/show.blade.php
@@ -22,35 +22,36 @@
     @endif
     @if ($data->show_users)
         <div class="grid">
-            <div class="g">
-                <div class="company clearfix">
-                    <div class="image g g-m-1-3">
-                        <img src="http://compostcreative.com/img/template/sharing.png" class="company_image" />
-                    </div>
-                    <div class="details g g-m-1-3">
-                        <h1>@if ($data->parent_company)
-                            <a href="{{ URL::route('companies_show', array(
-                                'key' => $data->parent_company->url_entity()
-                                )) }}">{{ $data->parent_company->name }}</a>:
-                            @endif
-                                {{ $data->company->name }}
-                        </h1>
-                        <p class="description">We are a creative studio producing visual effects and animation for TV, film and web. We love to bring stories &amp; ideas to life.</p>
-                        
-                        <h3>Contact details</h3>
-                        <p class="address">Buzzer 9, 24-26 Shacklewell Lane, London, E8 2EZ</p>
-                        <p class="url"><a href="http://compostcreative.com">http://compostcreative.com</a></p>
-                        <p class="email"><a href="mailto:info@compostcreative.com">info@compostcreative.com</a></p>
-                        <p class="telephone">0208 5467384</p>
-                        <p class="twitter">@compostcreative</p>
-
-                        <h3>Actions</h3>
-                        <ul>
-                            <li><a href="#">Recommend</a></li>
-                            <li><a href="#">Join</a></li>
-                        </ul>
-                    </div>
-                </div>
+            <div class="image g g-m-1-3">
+                <img src="http://compostcreative.com/img/template/sharing.png" class="company_image" />
+            </div>
+            <div class="details g g-m-2-3">
+                <h1>@if ($data->parent_company)
+                    <a href="{{ URL::route('companies_show', array(
+                        'key' => $data->parent_company->url_entity()
+                        )) }}">{{ $data->parent_company->name }}</a>:
+                    @endif
+                        {{ $data->company->name }}
+                </h1>
+                <p class="description">We are a creative studio producing visual effects and animation for TV, film and web. We love to bring stories &amp; ideas to life.</p>
+                <h3>Actions</h3>
+                <ul>
+                    <li><a href="#">Recommend</a></li>
+                    <li><a href="#">Join</a></li>
+                </ul>
+            </div>
+        </div>
+    @endif
+    @if ($data->show_users)
+        <div class="grid">
+            <div class="g contact">
+                <h2>Contact details</h3>
+                <p class="url"><a href="http://compostcreative.com">http://compostcreative.com</a></p>
+                <p class="email"><a href="mailto:info@compostcreative.com">info@compostcreative.com</a></p>
+                <p class="telephone">0208 5467384</p>
+                <p class="twitter">@compostcreative</p>
+                <p class="address">Buzzer 9<br>24-26 Shacklewell Lane<br>London<br>E8 2EZ</p>
+                
             </div>
         </div>
     @endif
@@ -68,6 +69,7 @@
             </div>
         </div>
     @endif
+    
     @if (Auth::user() && !$data->visitor_in_company)
         <p>
             <form method="post" action="{{ URL::route('company_membership', array(
