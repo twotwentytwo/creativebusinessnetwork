954cc660a8dfa5088feec8171625d282af271b8a
Tom Kershaw <tmkersh@gmail.com>
2014-11-09 18:47:58 +0000

    fixing col spacing on company

--- webroot/app/views/layouts/master.blade.php
+++ webroot/app/views/layouts/master.blade.php
@@ -12,39 +12,39 @@
         <!--[if lt IE 9]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
-        <div class="header situated--half">
-            <div class="grid">
-                <div class="g g-1-2">
-                    <div class="header__logo"><a href="{{ URL::route('home') }}">Creative Business Network</a></div>
-                </div>
-                <div class="g g-1-2">
-                    <div class="header__nav" id="nav">
-                        <nav>
-                        <ul class="list--unstyled text--right">
-                                <!--<li class="header__navitem header__search"><input type="search" placeholder="Search for a business or skillset" /><button type="submit"><i class="fa fa-search"></i></button></li>-->
-                            @if(Auth::check())
-                                <li class="header__navitem"><a href="{{ URL::route('user_dashboard', array('key' => Auth::user()->url_key())) }}">Dashboard</a></li>
-                                <li class="header__navitem"><a href="{{ URL::route('logout') }}"><i class="fa fa-sign-out"></i> Log out</a></li>
-                            @else
-                                <li class="header__navitem"><a href="{{ URL::route('login') }}"><i class="fa fa-sign-in"></i> Log in / Register</a></li>
-                            @endif
-                        </ul><!--
-                        --></nav><!--
-                    --></div><!-- end nav -->
+        
+            <div class="header situated--half">
+                <div class="grid">
+                    <div class="g g-1-2">
+                        <div class="header__logo"><a href="{{ URL::route('home') }}">Creative Business Network</a></div>
+                    </div>
+                    <div class="g g-1-2">
+                        <div class="header__nav" id="nav">
+                            <nav>
+                            <ul class="list--unstyled text--right">
+                                    <!--<li class="header__navitem header__search"><input type="search" placeholder="Search for a business or skillset" /><button type="submit"><i class="fa fa-search"></i></button></li>-->
+                                @if(Auth::check())
+                                    <li class="header__navitem"><a href="{{ URL::route('user_dashboard', array('key' => Auth::user()->url_key())) }}">Dashboard</a></li>
+                                    <li class="header__navitem"><a href="{{ URL::route('logout') }}"><i class="fa fa-sign-out"></i> Log out</a></li>
+                                @else
+                                    <li class="header__navitem"><a href="{{ URL::route('login') }}"><i class="fa fa-sign-in"></i> Log in / Register</a></li>
+                                @endif
+                            </ul><!--
+                            --></nav><!--
+                        --></div><!-- end nav -->
+                    </div>
                 </div>
             </div>
-        </div>
-
-        <div class="content">
-            @yield('content')
-        </div>
-
-        <div class="footer situated clearfix">
-            <p class="copyright">&copy; Creative Business Network 2014</p>
-            <ul class="legal list--unstyled">
-                <li><a href="#">Privacy policy</a></li>
-                <li><a href="#">Terms &amp; Conditions</a></li>
-            </ul>
-        </div>
+            <div class="content">
+                @yield('content')
+            </div>
+            <div class="footer situated clearfix">
+                <p class="copyright">&copy; Creative Business Network 2014</p>
+                <ul class="legal list--unstyled">
+                    <li><a href="#">Privacy policy</a></li>
+                    <li><a href="#">Terms &amp; Conditions</a></li>
+                </ul>
+            </div>
+       
     </body>
 </html>
