eb2e50f89a65c907770a62062c7da0a4262e11f1
Tom Kershaw <tom@twotwentytwo.co.uk>
2014-11-09 21:39:46 +0000

    Adding user page design

--- webroot/app/views/users/profile.blade.php
+++ webroot/app/views/users/profile.blade.php
@@ -1,21 +1,34 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="grid"><!--
-    --><div class="g">
-            @include('partials.messages')
+    
+
+    <div class="grid clearfix">
+        <div class="image g g-m-1-3">
+            <img src="https://scontent-a-ams.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/316129_10150875453480035_2023052290_n.jpg?oh=6b53ece98d7e9e28a797a1553cbe7cf9&oe=551DFCD0" class="company_image" />
+        </div>
+        <div class="details g g-m-2-3">
             <h1>{{ $data->user->getDisplayName() }}</h1>
-        </div><!--
-    --></div>
+            <p class="description">Web Developer &amp; Business Analyst</p>
+            <p class="url"><a href="http://compostcreative.com">http://twotwentytwo.co.uk</a></p>
+            <p class="telephone">0208 5467384</p>
+            <p class="address">London</p>
+            <ul class="recommend">
+                <li><a href="#" class="btn">Recommend</a></li>
+            </ul>
+        </div>
+    </div>
 
     @if ($data->show_companies)
-<h2>Companies</h2>
-<ul>
-    @foreach($data->companies_for_user as $company_for_user)
-    <li><a href="{{ URL::route('companies_show', array(
-                        'key' => $company_for_user->getCompany()->url_entity()
-                    )) }}">{{ $company_for_user->getCompany()->name }}</a></li>
-    @endforeach
-</ul>
+        
+            <h2>Companies</h2>
+            <ul>
+            @foreach($data->companies_for_user as $company_for_user)
+                <li><a href="{{ URL::route('companies_show', array(
+                            'key' => $company_for_user->getCompany()->url_entity()
+                        )) }}">{{ $company_for_user->getCompany()->name }}</a></li>
+            @endforeach
+            </ul>
+        
     @endif
 @stop
